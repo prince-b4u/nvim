38cc974b32259b2816a71efac44426386ff4c8ba
Prince Junior Mguni <princem@bestforu.co.za>
2023-05-06 08:48:30 +0200

    removed lua lsp

--- lua/after/lsp-zero-config.lua
+++ lua/after/lsp-zero-config.lua
@@ -1,14 +1,11 @@
 local lsp = require('lsp-zero').preset({})
 
 lsp.on_attach(function(client, bufnr)
-  lsp.default_keymaps({buffer = bufnr})
-lsp.buffer_autoformat()
-
+	lsp.default_keymaps({ buffer = bufnr })
+	lsp.buffer_autoformat()
 end)
 
 -- (Optional) Configure lua language server for neovim
-require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+--require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
 lsp.setup()
-
-
